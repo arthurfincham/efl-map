64ae496a9429927f582ae897a78037a7f3282365
Arthur Fincham <arthurfincham@me.com>
2023-12-04 22:49:10 +0000

    Add @vercel/analytics package

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import 'leaflet/dist/leaflet.css';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
